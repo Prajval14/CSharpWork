fe1014ab5143ce4516e20d8df85b4b1b52040b0f
Prajval Patel <prajval1420@gmail.com>
2024-03-07 09:48:36 -0500

    Real Estate Management System - Defining the residence and address class

---
+++ Work_3/Question_1/Address.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Question_1
+{
+    public class Address
+    {
+        private string Street;
+        private string Municipality;
+        private string Region;
+        private string PostalCode;
+
+        public Address()
+        {
+            Street = "Default Street";
+            Municipality = "Default Municipality";
+            Region = "Default Region";
+            PostalCode = "000000";
+        }
+
+        public Address(string street, string municipality, string region, string postalCode)
+        {
+            Street = street;
+            Municipality = municipality;
+            Region = region;
+            PostalCode = postalCode;
+        }
+
+        public void PrintAddress()
+        {
+            Console.WriteLine($"Street: {Street}\nCity: {Municipality}\nState: {Region}\nZip Code: {PostalCode}");
+        }
+    }
+}
